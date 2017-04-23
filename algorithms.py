33a8c2df565648943f44a1f95fe3b0dbcb5295d3
Evgeny <krivosheevevgeny@gmail.com>
2017-04-23 17:00:22 +0200

    feature/bubble_sort

--- algorithms.py
+++ algorithms.py
@@ -12,5 +12,16 @@ def binary_search(lst, item):
             high = mid - 1
     return None
 
+
+def bubble_sort(lst):
+    length = len(lst)
+    for i in range(length - 1):
+        for j in range(length - i - 1):
+            if lst[j] > lst[j+1]:
+                lst[j], lst[j+1] = lst[j+1], lst[j]
+    return lst
+
+
 if __name__ == '__main__':
-    print binary_search(range(100), 99)
+    # print binary_search(range(100), 99)
+    print bubble_sort([3,2,5,1,8,6,4,9,7])
