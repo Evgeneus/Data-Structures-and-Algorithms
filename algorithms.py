9bd09d7733e356229eb9d9ace99975235eced448
Evgeny <krivosheevevgeny@gmail.com>
2017-04-23 17:26:00 +0200

    selection sort

--- algorithms.py
+++ algorithms.py
@@ -22,6 +22,21 @@ def bubble_sort(lst):
     return lst
 
 
+def selection_sort(lst):
+    length = len(lst)
+    for i in range(length - 1):
+        ind_min = i
+        item_min = lst[ind_min]
+        for j in range(i+1, length):
+            if item_min > lst[j]:
+                ind_min = j
+        item_min = lst[ind_min]
+        lst[ind_min] = lst[i]
+        lst[i] = item_min
+    return lst
+
+
 if __name__ == '__main__':
     # print binary_search(range(100), 99)
-    print bubble_sort([3,2,5,1,8,6,4,9,7])
+    # print bubble_sort([3,2,5,1,8,6,4,9,7])
+    print selection_sort([3,2,5,1,8,6,4,9,7])
