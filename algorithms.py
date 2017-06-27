6e3c81ae3320e8eddf468739c79f7aeb6fbdff42
Evgeny <krivosheevevgeny@gmail.com>
2017-06-27 14:30:09 +0300

    merge sort

--- algorithms.py
+++ algorithms.py
@@ -68,6 +68,38 @@ def quicksort(lst):
         return quicksort(less) + equal + quicksort(greater)
 
 
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)/2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+
 # RECURSION
 def sum_rec(lst):
     if len(lst) == 1:
@@ -87,4 +119,6 @@ def max_element_rec(lst):
 
 if __name__ == '__main__':
     # print binary_search(range(100), 99)
-    print insertion_sort([3,2,5,1,8,6,8,4,9,0,7])
+    arr = [3,2,5,1,8,6,8,4,9,0,7]
+    merge_sort(arr)
+    print arr
